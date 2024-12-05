b15ebfa130e2c23c448821b6d4ad123330fb7d0b
Victor Perez <perezmolinavictoralfonso@gmail.com>
2024-12-05 09:36:34 -0500

    cambio de bd

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.listen(PORT, () => console.log('Server running port ' + PORT))
 app.get('/', (req, res) => {
   // Envía un "OK" como respuesta
 
-  res.send('OK PRODUCTION')
+  res.send('OK DEV')
 })
 app.post('/login', async (req, res) => {
   try {
